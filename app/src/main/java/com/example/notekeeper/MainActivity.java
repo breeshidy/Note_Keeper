94584e0aa7125c75daad03afa648efae9c9dc69e
Bridget <bridgetshidok@gmail.com>
2020-07-27 09:30:24 +0100

    Merge branch 'master' of https://github.com/breeshidy/Note_Keeper
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    Added the changes for the setting Activity

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.notekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -72,7 +75,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-
+        //saves default to the preference list but does not override it hence "false"
+        //initialise default values with our preferences
+        PreferenceManager.setDefaultValues(this, R.xml.general_preferences, false);
+        PreferenceManager.setDefaultValues(this, R.xml.messages_preferences, false);
+        PreferenceManager.setDefaultValues(this, R.xml.notification_preference, false);
+        PreferenceManager.setDefaultValues(this, R.xml.sync_preferences, false);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
 
@@ -111,7 +119,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onResume();
         //update the changes made to the fields
         mNoteRecyclerAdapter.notifyDataSetChanged();
+        updateNavHeader();
 
+    }
+
+    private void updateNavHeader() {
+
+        //get a reference for each view
+        mNavigationView = findViewById(R.id.nav_view);
+        View headerView = mNavigationView.getHeaderView(0);
+        TextView textUserName = headerView.findViewById(R.id.text_user_name);
+        TextView textEmailAddress = headerView.findViewById(R.id.text_email_address);
+
+        //interact with preference system to get the values of the text views
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        //pass in the key of the preference you want displayed, defValue shows if no preference with that jey is found
+        String userName = pref.getString("user_display_name", "");
+        String emailAddress = pref.getString("user_email_address", "");
+
+        //assign the "key values" to the text view
+        textUserName.setText(userName);
+        textEmailAddress.setText(emailAddress);
     }
 
     private void initializeDisplayContent() {
@@ -212,8 +240,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             Log.d(TAG, "onNavigationItemSelected: display courses");
 
         }else if (id == R.id.nav_share){
+            
+            handleShare();
 
-            handleSelection(R.string.nav_share_message);
+//            handleSelection(R.string.nav_share_message);
 
         }else if (id == R.id.nav_send){
 
@@ -225,5 +255,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
 
         return true;
+    }
+
+    //this get the current value of the user's favourite social from the preference screen
+    private void handleShare() {
+        View view = findViewById(R.id.list_items);
+        Snackbar.make(view, "Share to - " +
+                        PreferenceManager.getDefaultSharedPreferences(this).getString("user_favorite_social", ""),
+                Snackbar.LENGTH_LONG).show();
     }
 }
